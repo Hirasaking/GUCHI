0a85204c2f29823a0c3ad43197571240704752ab
WIN DESKTOP <asutoronoka@gmail.com>
2019-09-03 22:57:42 +0900

    不要なマイグレーションファイル削除

--- database/migrations/2019_08_25_151144_create_articles_table.php
+++ database/migrations/2019_08_25_151144_create_articles_table.php
@@ -22,6 +22,25 @@ class CreateArticlesTable extends Migration
         });
     }
 
+//     public function up()
+//    {
+//        Schema::create('articles', function (Blueprint $table) {
+//            $table->engine = 'InnoDB';
+//            $table->increments('id')->comment('投稿ID');
+//            $table->Integer('user_id')->unsigned()->comment('ユーザID');
+//            $table->string('body', 255)->comment('本文');
+//            $table->string('category', 30)->nullable()->comment('投稿カテゴリー');
+//            $table->timestamps();
+//            $table->softDeletesTz()->comment('削除日時');
+//            
+////            $table->foreign('user_id')->references('user_id')
+////                    ->on('users')->onDelete('cascade');
+//        });
+//        
+//        DB::statement("ALTER TABLE articles COMMENT '投稿マスター'");
+//
+//    }
+   
     /**
      * Reverse the migrations.
      *
