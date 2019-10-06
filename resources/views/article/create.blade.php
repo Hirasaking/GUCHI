efc9b1c2b3aba8f49ee650722a29c16db4121ebf
blueegg <blueegg@blueeggnoMacBook-Pro.local>
2019-10-06 11:38:04 +0900

    入力画面への戻る操作修正

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="ja">
   <head>
-    <title>Laravelチュートリアル</title>
+    <title>投稿画面</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
@@ -22,6 +22,15 @@
     <form action="/article/confirm" method="post">
       <div class="form-group">
           <label for="jobInput">職業</label>
+          @if ($errors->has('title'))
+              @foreach($errors->get('title') as $title_error)
+              <ul>
+                  <li>
+                      {{ $title_error }}
+                  </li>
+              </ul>
+              @endforeach
+          @endif
           <input type="text" name="job" class="form-control" id="InputJob" value="{{ old('job') }}">
       </div>
 <!--
@@ -31,7 +40,7 @@
 -->
       <div class="form-group">
           <label for="bodyInput">内容</label>
-          <textarea name="body" id="InputBody" class="form-control" rows="3" value="{{ old('body') }}"></textarea>
+          <textarea type='text' name="body" id="InputBody" class="form-control" rows="3" value="{{ old('body') }}">{{ old('body') }}</textarea>
       </div>
       @csrf
       <button type="submit" class="btn btn-primary">投稿する</button>
