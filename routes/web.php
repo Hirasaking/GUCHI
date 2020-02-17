050a677369d3561f9f09d31e115e1541f8e5f189
asutoronoka <asutoronoka@gmail.com>
2020-02-17 23:32:10 +0900

    ホーム変更

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 
 Route::get('item', 'ItemController@index')->name('index');
-Route::get('/items/create', 'ItemController@create')->name('create');
-Route::post('/items', 'ItemController@store')->name('store');
+// Route::get('/items/create', 'ItemController@create')->name('create');
+// Route::post('/items', 'ItemController@store')ç
 Route::get('/items/{id}', 'ItemController@show')->name('show')->where('id', '[0-9]+');
 Route::get('/items/{id}/edit', 'ItemController@edit')->name('edit')->where('id', '[0-9]+');
 Route::patch('/items/{id}', 'ItemController@update')->name('update')->where('id', '[0-9]+');
@@ -21,12 +21,13 @@ Route::get('/items/{item}/delete', 'ItemController@delete')->name('delete')->whe
 Route::delete('/items/{item}', 'ItemController@destroy')->name('destroy')->where('item', '[0-9]+');  //追加
 
 
-Route::get('/', function () {
+Route::get('welcome', function () {
     return view('welcome');
 });
 
 // 投稿関連
-Route::get('article', 'ArticleController@index');
-Route::get('article-create', 'ArticleController@create')->name('article-create');
+Route::get('/', 'ArticleController@index');
+Route::get('article-create', 'ArticleController@create')->name('create');
+Route::post('article-create', 'ArticleController@store')->name('store');
 Route::get('article-confirm', 'ArticleController@confirm')->name('article-confirm');
 Route::get('article-complete', 'ArticleController@complete')->name('article-complete');
