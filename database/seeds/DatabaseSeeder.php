e84c6278c98b0bf2b1ab1fbadb7d10a9eee58663
asutoronoka <asutoronoka@gmail.com>
2020-02-22 18:02:17 +0900

    実行順変更

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ArticlesTableSeeder::class);
-        $this->call(CommentsTableSeeder::class);
-        $this->call(ItemsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(UserInfosTableSeeder::class);
+        $this->call(ItemsTableSeeder::class);
+        $this->call(ArticlesTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
     }
 }
