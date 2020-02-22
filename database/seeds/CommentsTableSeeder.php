1a35f28085e5eb5f408d82ec8ba3b50712f60dc2
asutoronoka <asutoronoka@gmail.com>
2020-02-23 07:40:01 +0900

    データ追加

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -17,23 +17,37 @@ class CommentsTableSeeder extends Seeder
 
         DB::table($table_name)->insert([
             [
-                'article_id' => '1',
+                'article_id' => 1,
                 'body'       => 'コメントされた文章1',
                 'status'    => 0,
                 'created_at' => '2020-02-20 00:00:00',
                 'updated_at' => '2020-02-20 00:00:00',
             ],
             [
-                'article_id' => '2',
+                'article_id' => 2,
                 'body'       => 'コメントされた文章2',
                 'status'    => 0,
                 'created_at' => '2020-02-20 00:00:00',
                 'updated_at' => '2020-02-20 00:00:00',
             ],
             [
-                'article_id' => '2',
+                'article_id' => 2,
                 'body'       => 'コメントされた文章3',
                 'status'    => 0,
+                'created_at' => '2020-02-20 00:00:00',
+                'updated_at' => '2020-02-20 00:00:00',
+            ],
+            [
+                'article_id' => 99,
+                'body'       => '存在しない投稿IDのコメント',
+                'status'    => 0,
+                'created_at' => '2020-02-20 00:00:00',
+                'updated_at' => '2020-02-20 00:00:00',
+            ],
+            [
+                'article_id' => 2,
+                'body'       => '存在しない投稿IDのコメント',
+                'status'    => 1,
                 'created_at' => '2020-02-20 00:00:00',
                 'updated_at' => '2020-02-20 00:00:00',
             ],
