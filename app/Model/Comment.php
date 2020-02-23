de04a7e0be54d74497f39eafc51ce8070524ed20
asutoronoka <asutoronoka@gmail.com>
2020-02-24 00:41:48 +0900

    取得処理追加

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     // 投稿番号ごとにコメントを取得
-    public function getComment(){
-        $query = Comment::whereNull('deleted_at')
-        ->whereNotIn('status', [0, 2]) //確認中・削除コメント除く
+    /**
+     * 
+     * @return array $query
+     */
+    public function getComment($article_id_list){
+        $comment_list = Comment::whereNull('deleted_at')
+        ->whereIn('article_id', $article_id_list) //投稿IDの存在するコメントが対象
+        // ->whereNotIn('status', [0, 2]) //確認中・削除コメント除く
         ->select(
             'article_id',
             'body',
@@ -17,6 +22,13 @@ class Comment extends Model
             )
         ->get();
 
+        $data = []; // コメントデータを初期化
+        foreach($comment_list as $key => $comment){
+            // 投稿番号をキー、内容を要素に追加
+            // 同じ投稿へのコメントはまとめる
+            $data[$comment->article_id][] = $comment->body;
+        }
+        $query = collect($data);
         return $query;
     }
 
